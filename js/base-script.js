8dd52d0ac4e1fa75350132524d0377532955c474
Peter <jardine.peter.94@gmail.com>
2021-07-20 14:05:31 +0100

    changed the typewriter messages

--- js/base-script.js
+++ js/base-script.js
@@ -1,3 +1,3 @@
 
 let $elements = $(".text-typewriter-container .typewriter--text");
-$(".text-typewriter-output").tickerText($elements, 6, 3, 30, 0, 0.5, 2,false,"timerpause","timerstop");
+$(".text-typewriter-output").tickerText($elements, 5, 3, 30, 0, 0.5, 2,false,"timerpause","timerstop");
