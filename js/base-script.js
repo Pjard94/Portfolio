2043b7019028713d93b0859eb665b593a88de96e
Peter <jardine.peter.94@gmail.com>
2021-07-21 17:09:46 +0100

    Installing a new typewriter plugin

--- js/base-script.js
+++ js/base-script.js
@@ -1,3 +1,10 @@
 
-let $elements = $(".text-typewriter-container .typewriter--text");
-$(".text-typewriter-output").tickerText($elements, 5, 6, 40, 0, 0.5, 2,false,"timerpause","timerstop");
+// let $elements = $(".text-typewriter-container .typewriter--text");
+// $(".text-typewriter-output").tickerText($elements, 5, 6, 40, 0, 0.5, 2,false,"timerpause","timerstop");
+
+$('.text-typewriter-output').typeWrite({
+    speed: 30,
+    repeat: false,
+    cursor: true,
+    color: 'white',
+  })
