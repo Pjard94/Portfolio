54266a423b6ece7c8f06f1294d0aa38632aec0f8
ChaosMiner65 <82098806+Pjard94@users.noreply.github.com>
2021-07-22 09:42:55 +0100

    Changed jQuery plugin to work on IE

--- js/base-script.js
+++ js/base-script.js
@@ -2,9 +2,13 @@
 // let $elements = $(".text-typewriter-container .typewriter--text");
 // $(".text-typewriter-output").tickerText($elements, 5, 6, 40, 0, 0.5, 2,false,"timerpause","timerstop");
 
-$('.text-typewriter-output').typeWrite({
-    speed: 30,
-    repeat: false,
-    cursor: true,
-    color: 'white',
-  })
+var typed = new Typed('.text-typewriter-output', {
+  strings: ["I'm a Web Developer", "I'm a Programmer", "I'm a Designer"],
+  showCursor: false,
+  typeSpeed: 100,
+  smartBackspace: true,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+  loopCount: Infinity
+});
