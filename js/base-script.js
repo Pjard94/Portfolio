7feb046b40e2ddd788b9cf08c9e4e14dd01ace2e
ChaosMiner65 <82098806+Pjard94@users.noreply.github.com>
2021-07-22 15:00:55 +0100

    Added notes and some fixes as suggested

--- js/base-script.js
+++ js/base-script.js
@@ -1,14 +1,15 @@
-
-// let $elements = $(".text-typewriter-container .typewriter--text");
-// $(".text-typewriter-output").tickerText($elements, 5, 6, 40, 0, 0.5, 2,false,"timerpause","timerstop");
-
+//This is the text typewriter code
 var typed = new Typed('.text-typewriter-output', {
   strings: ["I'm a Web Developer", "I'm a Programmer", "I'm a Designer"],
+  // these are the sentences that get typed out
   showCursor: false,
+  // this disabled the standard cursor
   typeSpeed: 100,
   smartBackspace: true,
   backSpeed: 100,
   backDelay: 1000,
+  // These set the speed and the amount of characters removed
   loop: true,
   loopCount: Infinity
+  // These rules set the looping aspect so that the typewriter never stops
 });
