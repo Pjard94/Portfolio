5c6e38f62c76ba8dff175ed97cd308baccc56bb9
ChaosMiner65 <82098806+Pjard94@users.noreply.github.com>
2021-08-05 16:01:08 +0100

    Added validation

--- js/base-script.js
+++ js/base-script.js
@@ -13,3 +13,39 @@ var typed = new Typed('.text-typewriter-output', {
   loopCount: Infinity
   // These rules set the looping aspect so that the typewriter never stops
 });
+
+
+// This will be for the form submission
+const inputs = document.getElementsByClassName("contact-form-input");
+const email = document.getElementById("client-email");
+const message = document.getElementById("message")
+const emptyREGEX = /^[a-zA-Z0-9]+$/;
+const messageREGEX = /^[a-zA-Z0-9]+\\p{Punct}+\\s{Space}+$/;
+const emailREGEX = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+[.][a-zA-Z]{2,6}$/;
+
+$(inputs).focus(function() {
+  this.classList.add("Form-focused");
+  console.log('in');
+}).blur(function() {
+  this.classList.remove("Form-focused");
+  this.classList.remove("Form-valid");
+  this.classList.remove("Form-invalid");
+  if (this == email) {
+    if (emailREGEX.test(this.value) == false) {
+      this.classList.add("Form-invalid")
+    } else {
+      this.classList.add("Form-valid")
+    }
+  } else if (this == message) {
+    this.classList.add("Form-valid")
+  }else if (emptyREGEX.test(this.value) == false) {
+    this.classList.add("Form-invalid")
+  } else {
+    this.classList.add("Form-valid")
+  }
+  console.log(this.value);
+});
+
+document.getElementById("contact-form-submit").addEventListener("click", function validateForm() {
+  
+}) 
---
+++ js/nav-menu.js
@@ -0,0 +1,7 @@
+const flyoutMenu = document.getElementById("big-nav");
+
+
+
+document.getElementById("burger-box").addEventListener("click", function showMenu() {
+    flyoutMenu.classList.add("show")
+});
