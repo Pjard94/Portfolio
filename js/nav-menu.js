589dd56c8fad95f96aa579b4aaba6982a0d351a7
ChaosMiner65 <82098806+Pjard94@users.noreply.github.com>
2021-08-06 14:19:30 +0100

    Fixing the menus

--- js/nav-menu.js
+++ js/nav-menu.js
@@ -1,7 +1,15 @@
 const flyoutMenu = document.getElementById("big-nav");
-const burger = document.getElementById("burger-box")
+const burger = document.getElementById("burger-box");
+const page = document.getElementById("page");
 
 burger.addEventListener('click', function showMenu() {
-    flyoutMenu.classList.add("show")
-    this.classList.add("hide")
+    flyoutMenu.classList.add("show");
+    this.classList.add("hide");
+    page.classList.add("menu-showing");
 });
+
+page.addEventListener('click', function hideMenu() {
+    flyoutMenu.classList.remove("show");
+    burger.classList.remove("hide");
+    page.classList.remove("menu-showing");
+})
