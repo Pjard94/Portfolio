8de9976fd0d44931f6740a64ba8daf3b71d5675c
ChaosMiner65 <82098806+Pjard94@users.noreply.github.com>
2021-08-09 17:02:09 +0100

    Fixed sizing and margins/paddings

--- js/nav-menu.js
+++ js/nav-menu.js
@@ -1,8 +1,9 @@
 const burger = document.getElementById("burger-box");
 const page = document.getElementById("menu-showing");
 const body = document.body;
-let menuCheck = 0;
+let menuCheck = 0; //Allows to have an easier switch with the the button thanks IE
 
+// This works in all the browsers some the more easily used functionality doesn't work in IE
 burger.addEventListener('click',function showMenu() {
     if (menuCheck === 0) {
         menuCheck = 1;
@@ -13,6 +14,9 @@ burger.addEventListener('click',function showMenu() {
     }
 });
 
+
+// This allows for a click anywhere on the page to remove the menu
 page.addEventListener('click', function hideMenu() {
-    document.body.classList.remove('Nav-out');
+    body.classList.remove('Nav-out');
+    menuCheck = 0;
 })
